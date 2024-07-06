77ea29de0a19bb3972972400bba0e08415e7c0d1
Shreyas Vaidya <shreyas.vaidya@sunking.com>
2024-07-06 19:48:40 +0530

    feat: Added the product update route handlers.

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,13 @@ import {
   getProducts,
   updateProduct,
 } from "./handlers/product";
+import {
+  createUpdate,
+  deleteUpdate,
+  getOneUpdate,
+  getUpdates,
+  updateUpdate,
+} from "./handlers/update";
 const router = Router();
 
 /**
@@ -32,18 +39,24 @@ router.delete("/product/:id", deleteProduct);
 /**
  * Updates
  */
-router.get("/update", () => {});
-router.get("/update/:id", () => {});
+router.get("/update", getUpdates);
+router.get("/update/:id", getOneUpdate);
 router.put(
   "/update/:id",
   body("title").optional(),
   body("body").optional(),
-  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
   body("version").optional(),
-  () => {}
+  updateUpdate
 );
-router.post("/update", body("title").exists(), body("body").exists(), () => {});
-router.delete("/update/:id", () => {});
+router.post(
+  "/update",
+  body("title").exists(),
+  body("body").exists(),
+  body("productId").exists().isString(),
+  createUpdate
+);
+router.delete("/update/:id", deleteUpdate);
 
 /**
  * Update Points
